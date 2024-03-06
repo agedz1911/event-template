b5e4c740e21453659d83e30d18de2b659e0fbf69
agedz1911 <asep.cgeds@gmail.com>
2024-03-07 06:23:21 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.mainLayout')
+
+@section('title', 'HOME')
+
+@section('content')
+
+<div>
+    <h1>Ini Home page</h1>
+    <button class="btn btn-primary">submit</button>
+</div>
+@endsection
